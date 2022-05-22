3c196c6ae2f93c091131fdb2343efcfbbaaf8a51
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-22 20:23:28 +0300

    Initial Commit

---
+++ FancyWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FancyWeatherApp
+//
+//  Created by stickerfinger489 on 22/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FancyWeatherApp/FancyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyWeatherAppApp.swift
+//  FancyWeatherApp
+//
+//  Created by stickerfinger489 on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FancyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
