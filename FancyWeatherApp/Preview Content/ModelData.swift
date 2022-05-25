e834e7af4e9bef8cd0427c55208f16bc3622c739
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-25 23:08:36 +0300

    Final sample weather app after fixing bugs

---
+++ FancyWeatherApp/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  FancyWeatherApp
+//
+//  Created by stickerfinger489 on 25/05/2022.
+//
+
+import Foundation
--- FancyWeatherApp/Views/ContentView.swift
+++ FancyWeatherApp/Views/ContentView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
     var body: some View {
-        @StateObject var locationManager = LocationManager()
-        
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
